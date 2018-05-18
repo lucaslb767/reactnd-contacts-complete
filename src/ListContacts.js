bbf340ff5af6ed60cb8b5d5b54764d1f168c6167
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-05-18 14:43:18 -0300

    faz botao de remover contatos remover o state

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -12,7 +12,7 @@ function ListContacts (props) {
                         <p>{contact.name}</p>
                         <p>{contact.email}</p>
                     </div>
-                    <button className='contact-remove'></button>
+                    <button onClick={()=>props.onDeleteContact(contact)} className='contact-remove'></button>
 
                 </li>
             )}
